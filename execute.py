d5f54ca6ea2c75dca429802dc1e170902410c8f0
invicta198 <tejasmanas39@gmail.com>
2020-05-11 20:58:04 +0530

    relocation and user input via click module

--- execute.py
+++ execute.py
@@ -1,7 +1,14 @@
-import flipkart
-import ebay
-if __name__ == '__main__':
-    # product = input("ENTER PRODUCT : ").strip()
-    product = "earphone"
+import click
+from scrape import ebay, flipkart
+
+
+@click.command()
+@click.option('--product', default='laptop', prompt="ENTER PRODUCT ", help='Name of Item you Wish to Search')
+def start_task(product):
+    product = product.strip()
     flipkart.start_task(product)
     ebay.start_task(product)
+
+
+if __name__ == '__main__':
+    start_task()
---
+++ scrape/flipkart.py
@@ -0,0 +1,77 @@
+import bs4
+# from bs4 import BeautifulSoup
+import requests
+import urllib
+
+
+def first_method(product_details):
+    for product_detail in product_details:
+        data = product_detail.findAll("div", {"class": "col col-7-12"})
+        for datum in data:
+
+            name = datum.div
+            print("Product : {}".format(name.text))  # PRINTING NAME OF EACH PRODUCT FOUND
+
+            try:
+                rating = datum.find("div", {"class": "hGSR34"})
+                print("Rating : {}".format(rating.text))  # PRINTING RATING OF EACH PRODUCT FOUND
+            except Exception as e:
+                print("")
+                # print("Error Occurred {}".format(e))
+
+            detail = datum.findAll("li", {"class": "tVe95H"})
+            for k in detail:
+                print("Details : {}".format(k.text))  # PRINTING DETAILS OF EACH PRODUCT FOUND
+
+            print("********************")
+
+
+def second_method(content):
+    product_line = content.findAll("div", {"class": "_3O0U0u"})
+    print("Total Products : {}".format(len(product_line)))
+    for i in product_line:
+        for j in i:
+
+            product_name = j.div.find("a", {"class": "_2cLu-l"})['title']
+            print("Product : {}".format(product_name))  # PRINTING NAME OF EACH PRODUCT FOUND
+
+            try:
+                rating = j.div.find("div", {"class": "niH0FQ _36Fcw_"}).text
+                print("Rating(Number) : {}".format(rating))  # PRINTING RATING OF EACH PRODUCT FOUND
+
+                details = j.div.find("a", {"class": "_1Vfi6u"}).text
+                print("Details : {}".format(details))  # PRINTING DETAILS OF EACH PRODUCT FOUND
+
+            except Exception as e:
+                print("")
+                # print("Error Occurred {}".format(e))
+
+            print("********************")
+
+
+def start_task(product):
+    product = urllib.parse.quote_plus(product, safe="", encoding=None, errors=None)
+    my_url = "https://www.flipkart.com/search?q={}".format(product)
+    html = None
+
+    try:
+        response = requests.get(my_url)
+
+        if response.status_code == 200:
+            html = response.content.decode()
+
+        page = bs4.BeautifulSoup(html, "html.parser")
+
+        details = page.find_all("div", {"class": "_1-2Iqu row"})
+
+        if len(details) != 0:
+            first_method(details)
+        else:
+            second_method(page)
+
+    except Exception as e:
+        print("Error Occurred {}".format(e))
+
+
+if __name__ == '__main__':
+    start_task()
